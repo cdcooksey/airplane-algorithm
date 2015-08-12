eb5f6c7ca174974d14031a5eb4c50be4730a3c4e
Charlie Cooksey <cdcooksey@gmail.com>
2015-08-11 20:52:34 -0500

    Adds comment.

--- app/directives/plane-form.js
+++ app/directives/plane-form.js
@@ -6,6 +6,11 @@ angular.module('myApp.directives.plane-form', [])
       templateUrl: 'templates/plane-form.html',
       controller: ['$scope', 'Plane', function ($scope, Plane) {
 
+        /**
+         * Pulls an object containing all possible
+         * sizes and types from factories/plane.js
+         * @type {plane}
+         */
         $scope.plane = Plane.plane;
 
       }]
